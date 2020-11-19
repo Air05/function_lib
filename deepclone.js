2d7a3485e5da53e025f6d6e8efb719930c0f992b
chtholly <271268970@qq.com>
2020-11-19 17:15:04 +0800

    新增深拷贝函数

---
+++ deepclone.js
@@ -0,0 +1,22 @@
+/**
+ *
+ *一个简单的深拷贝
+ * @param {*} target
+ * @param {*} [map=new Map()]
+ * @returns
+ */
+function clone(target, map = new Map()) {
+    if (typeof target === 'object') {
+        let cloneTarget = Array.isArray(target) ? [] : {};
+        if (map.get(target)) {
+            return map.get(target);
+        }
+        map.set(target, cloneTarget);
+        for (const key in target) {
+            cloneTarget[key] = clone(target[key], map);
+        }
+        return cloneTarget;
+    } else {
+        return target;
+    }
+};
